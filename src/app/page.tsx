16e887405c0cc3c728fdcad8e779fe1a87321c40
Kleber Coelho <klebercoelhogt@gmail.com>
2024-06-03 21:45:39 -0300

    adicionando componente de produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,27 @@
 import Image from "next/image";
+import { ProductType } from "@/types/ProductType";
+import Product from "./components/Product";
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
 
-export default function Home() {
+ 
+  if (!res.ok) {
+   
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+export default async function Home() {
+  const products = await getProducts()
+
+
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          prod 1
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          prod 1
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          prod 1
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          prod 1
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          prod 1
-        </div>
-        
+        {products.map((product: ProductType) =>(
+          <Product key={product.id} product={product}> </Product>
+        ))}        
       </div>
       <h1>teste</h1>
     </div>
