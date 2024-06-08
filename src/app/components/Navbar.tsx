028cfb4399acc2bf1b9830588e998dc8dd6b2c79
Kleber Coelho <klebercoelhogt@gmail.com>
2024-06-07 21:17:51 -0300

    criado o carrinho

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
+import Cart from './Cart'
 
 export default function Navbar() {
   return (
@@ -9,22 +10,7 @@ export default function Navbar() {
         Next store
       </Link>
       <div className='flex items-center gap-8'>
-        <div className='flex items-center cursor-pointer relative'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-          </svg>
-          <span className='
-          bg-teal-600  
-          text-sm font-bold 
-          rounded-full 
-          h-5 w-5 
-          flex items-center justify-center 
-          absolute
-          left-3
-          bottom-3 
-          '>2</span>
-
-        </div>
+        <Cart />
         <div>
           <SignedIn >
             <UserButton/>
