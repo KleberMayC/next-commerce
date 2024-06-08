f52fa0d8c4567d8aabbf5bdab2923efb7f67a52e
Kleber Coelho <klebercoelhogt@gmail.com>
2024-06-07 21:04:49 -0300

    add button card

---
+++ src/store.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { ProductType } from "@/types/ProductType";
+import { persist } from "zustand/middleware";
+
+type CartState = {
+    cart: ProductType[]
+    // addToCart: (product: ProductType) => void
+    // removeFromCart: (productId: string) => void
+    isOpen: boolean
+    toggleCart: () => void
+}
+
+export const useCartStore = create<CartState>()(
+    persist((set) =>({
+        cart:[],
+        isOpen: false,
+        toggleCart: () => set((state) => ({isOpen: !state.isOpen}))
+    }),{name: 'cart-storage'})
+)
