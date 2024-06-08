6a60da3da99ff0e6abc14596209a035d95b87ba7
Kleber Coelho <klebercoelhogt@gmail.com>
2024-06-07 21:35:09 -0300

    criando a função para add no cart

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,7 +1,7 @@
 import { ProductType } from "@/types/ProductType"
-import Image from "next/image"
 import ProductImage from "./ProductImage"
 import { formatPrice } from "@/lib/utils"
+import AddCart from './AddCart'
 
 type ProductProps ={
     product: ProductType
@@ -19,7 +19,7 @@ export default function Product({ product} : ProductProps) {
         </p>
 
       </div>
-      <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5  text-sm text-center">Adicionar ao Carrinho</button>
+      <AddCart product={product}/>
     </div>
   )
 }
